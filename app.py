bbb9cb2a6be3d7d0fc86c11fe1685641362eff10
Josh Edgcomb <joshedgcomb@yahoo.com>
2013-04-04 20:33:25 -0700

    a

--- app.py
+++ app.py
@@ -8,22 +8,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {'author': {'nickname': "Steve"},
-         'body': 'Fuck you, Josh'},
-        {'author': {'nickname': "Josh"},
-         'body': 'Steve is an asshole!'}
-    ]
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template('404.html')
 
 
 if __name__ == '__main__':
